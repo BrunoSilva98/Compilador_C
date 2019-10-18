8a9e54943417c5972d0579daf9e6c26a906ea572
BrunoSilva98 <brunops98@hotmail.com>
2019-10-17 21:16:34 -0300

    Ajustes gerais

--- Analisador_Sintatico.py
+++ Analisador_Sintatico.py
@@ -1,4 +1,5 @@
 from Analisador_Lexico import AnalisadorLexico
+from tabulate import tabulate
 
 class Regra:
     def __init__(self, variavel):
@@ -17,18 +18,34 @@ class AnalisadorSintatico:
         self.cadeia = tokens
         self.cadeia.append('$')
         self.pilha = list()
-    
+        self.terminais = ['id','num','+','-','*','/','(',')','$']
+        self.tabelaPrintar = [["Pilha", "Cadeia", "Regra"]]
+
     def getNaoTerminal(self, variavel):
         for regra in self.tabela:
             if (regra.naoTerminal == variavel):
                 return regra
 
     def empilha(self, regra):
-        self.pilha.insert(0, regra[::-1])
+        if regra in self.terminais:
+            self.pilha.append(regra)
+        else:
+            for elemento in regra[::-1]:
+                self.pilha.append(elemento)
 
     def desempilha(self):
-        return self.pilha.pop(0)
+        return self.pilha.pop(len(self.pilha)-1)
         
+    def removeFirstCharCadeia(self):
+        self.cadeia = self.cadeia[1:]
+
+    def verificaRegra(self, naoVariavel, terminal):
+        regra = self.getNaoTerminal(naoVariavel)
+        lista = regra.terminais.keys()
+        if(terminal in lista):
+            return True
+        return False
+
     def criaTabelaRegras(self):
         E = Regra('E')
         E.addTerminal('id', 'TS')
@@ -64,9 +81,42 @@ class AnalisadorSintatico:
         F.addTerminal('(', '(E)')
         self.tabela.append(F)
     
-    def analiseSintatica(self):
-        pass
+    def printarCadeia(self):
+        texto = ""
+        for i in range(len(self.cadeia)-1):
+            texto = texto + self.cadeia[i][1]
+        return texto + '$'
 
+    def analiseSintatica(self):
+        topo = self.pilha[len(self.pilha)-1]
+        topoCadeia = self.cadeia[0][1]
+
+        while (topo != '$'):
+            topo = self.pilha[len(self.pilha)-1]
+
+            if(topo in self.terminais):
+                if(topo == topoCadeia):
+                    self.tabelaPrintar.append([list(self.pilha), self.printarCadeia(), 'RECONHECE'])
+                    self.desempilha()
+                    self.removeFirstCharCadeia()
+                else:
+                    self.tabelaPrintar.append([list(self.pilha), self.printarCadeia(), 'ERRO'])
+                    break
+            
+            elif(self.verificaRegra(topo,topoCadeia)):
+                regra = self.getNaoTerminal(topo)
+                self.tabelaPrintar.append([list(self.pilha), self.printarCadeia(), topo + '->' + regra.terminais[topoCadeia]])
+                self.desempilha() 
+                self.empilha(regra.terminais[topoCadeia])
+            
+            else:
+                self.tabelaPrintar.append([list(self.pilha), self.cadeia, 'ERRO'])
+                break
+    
+    def mostrarTabela(self):
+        print(tabulate(self.tabelaPrintar))
+
+                
 
 if __name__ == "__main__":
     analisadorLexico = AnalisadorLexico(r"C:\Users\bruno\Desktop\Projetos\Compilador\codigo.txt")    
@@ -74,4 +124,7 @@ if __name__ == "__main__":
 
     a = AnalisadorSintatico(analisadorLexico.listaTokens)
     a.criaTabelaRegras()
-    a.pilha.append(a.getNaoTerminal('E'))
+    a.pilha.append('$')
+    a.pilha.append(a.getNaoTerminal('E').naoTerminal)
+    a.analiseSintatica()
+    a.mostrarTabela()
